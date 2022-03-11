1a608f94efdd136bd6aed8998bb5f146ef1f770b
Blazej Duszynski <bduszynski92@gmail.com>
2022-03-11 19:05:17 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const buttons = document.querySelectorAll(".container_gallery--button");
+
+//const previousButton = document.querySelector(".container_gallery--previousButton");
+//const nextButton = document.querySelector(".container_gallery--nextButton");
+
+console.log(buttons);

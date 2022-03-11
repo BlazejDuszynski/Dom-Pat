f160098fdcda4f260ff2de03c20830e541578e74
Blazej Duszynski <bduszynski92@gmail.com>
2022-03-11 20:08:15 +0100

    switching photos within galery working

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 const buttons = document.querySelectorAll(".container_gallery--button");
 
-//const previousButton = document.querySelector(".container_gallery--previousButton");
-//const nextButton = document.querySelector(".container_gallery--nextButton");
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.className === "container_gallery--nextButton" ? 1 : -1;
+        const slides = button.closest(".container_gallery").querySelector(".container_gallery--slides");
 
-console.log(buttons);
-console.log(buttons);
+        const activeSlide = slides.querySelector(".activeSlide");
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if (newIndex < 0) {
+            newIndex = slides.children.length - 1
+        }
+        if (newIndex >= slides.children.length) {
+            newIndex = 0
+        }
+        slides.children[newIndex].classList.add("activeSlide")
+        activeSlide.classList.remove("activeSlide")
+    })
+})
+
